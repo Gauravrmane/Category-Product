03d5358fdc0e388fdeee6cacec1402e65f4d0b99
Gauravrmane <154352235+Gauravrmane@users.noreply.github.com>
2024-11-26 15:19:22 +0530

    Add files via upload

--- src/main/java/com/example/CarsInfo/Controller/ProductController.java
+++ src/main/java/com/example/CarsInfo/Controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.example.CarsInfo.Controller;
 
 import com.example.CarsInfo.Entity.Product;
-import com.example.CarsInfo.Service.Services;
+import com.example.CarsInfo.Service.CategoryServices;
+import com.example.CarsInfo.Service.ProductServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,37 +16,44 @@ import java.util.Optional;
 public class ProductController {
 
    @Autowired
-    private Services services;
+    private ProductServices productServices;
 
    @PostMapping("/saveproduct")
    public Product saveProduct(@RequestBody Product product){
 
-       return services.saveProduct(product);
+       return productServices.saveProduct(product);
    }
 
 
-   @GetMapping("/getAllProduct")
-   public List<Product> getAllProduct(){
-
-       return  services.getAllProduct();
-   }
+//   @GetMapping("/getAllProduct")
+//   public List<Product> getAllProduct(){
+//
+//       return  productServices.getAllProduct();
+//   }
+@GetMapping("/getAllProductPaged")
+public Page<Product> getAllProductPaged(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size) {
+    Pageable pageable = PageRequest.of(page, size);
+    return productServices.getAllProduct(pageable);
+}
 
    @GetMapping("/getproductById/{id}")
    public Optional<Product> getproductById(@PathVariable Long id ){
-        return services.getproductById(id);
+        return productServices.getproductById(id);
    }
 
    @PutMapping("/updateProductById/{id}")
    public Optional<Product> updateProductById(@PathVariable Long id ,@RequestBody Product product){
 
-       return services.updateProductById(id,product);
+       return productServices.updateProductById(id,product);
    }
 
 
    @DeleteMapping("/deleteProductById/{id}")
    public void deleteProductById (@PathVariable Long id){
 
-       services.deleteProductById(id);
+       productServices.deleteProductById(id);
 
    }
 
